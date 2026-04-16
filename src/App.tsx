c98a5f3f2763f007003099754331618516b686c3
ChiaMin <chen.chiamin@gmail.com>
2026-04-16 14:12:02 +0800

    fix: add aria-describedby attribute for improved accessibility

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import qrCodeImage from './assets/image-qr-code.png';
 function App() {
   return (
     <main className="bg-white font-outfit w-[320px] p-4 rounded-[20px] shadow-card">
-      <img className="rounded-[10px]" src={qrCodeImage} alt="QR code" />
-      <section className="text-center px-4 py-6 flex flex-col gap-4">
+      <img className="rounded-[10px]" src={qrCodeImage} alt="QR code" aria-describedby="qr-desc" />
+      <section id="qr-desc" className="text-center px-4 py-6 flex flex-col gap-4">
         <h1 className="text-[22px]/[1.2] font-bold text-slate-dark">
           Improve your front-end skills by building projects
         </h1>
