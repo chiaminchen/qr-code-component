0156aaf3ddf97f85a2e8c44fff56897a9ad8416a
ChiaMin <chen.chiamin@gmail.com>
2026-04-17 11:22:26 +0800

    fix: update favicon path and improve layout styles for better responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,18 @@ import qrCodeImage from './assets/image-qr-code.png';
 
 function App() {
   return (
-    <main className="bg-white font-outfit w-[320px] p-4 rounded-[20px] shadow-card">
+    <main className="bg-white font-outfit w-full max-w-xs p-4 rounded-card shadow-card">
       <figure>
-        <img className="rounded-[10px]" src={qrCodeImage} alt="QR code" />
+        <img className="rounded-image" src={qrCodeImage} alt="QR code leading to Frontend Mentor" />
         <figcaption className="sr-only">
           Scan this QR code to add the contact details to your phone.
         </figcaption>
       </figure>
       <section className="text-center px-4 py-6 flex flex-col gap-4">
-        <h1 className="text-[22px]/[1.2] font-bold text-slate-dark">
+        <h1 className="text-title leading-tight font-bold text-slate-dark">
           Improve your front-end skills by building projects
         </h1>
-        <p className="text-[15px]/[1.4] tracking-[0.2px] text-slate-medium">
+        <p className="text-body leading-relaxed tracking-wider text-slate-medium">
           Scan the QR code to visit Frontend Mentor and take your coding skills to the next level
         </p>
       </section>
